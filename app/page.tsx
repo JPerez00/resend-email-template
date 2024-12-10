39b654842c38aa529f31c217344b791effd9fe0f
Jorge Perez <information.jperez@gmail.com>
2024-12-10 12:08:57 +0800

    nav doc button added, page readability improved

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,14 @@ function Pin(props: JSX.IntrinsicAttributes & React.AnchorHTMLAttributes<HTMLAnc
 
 export default function Home() {
   return (
-    <main className="mb-20 flex flex-1 w-full flex-col items-center justify-center text-center">
-      <div className="shadow-sm font-semibold mb-4 first-letter:shadow inline-flex rounded-lg px-4 py-1 md:py-2 text-xs md:text-sm leading-6 text-zinc-400 bg-zinc-800 ring-1 backdrop-blur ring-white/20 hover:ring-white/30">
+    <main className="mb-20 mt-8 md:mt-12 flex flex-1 w-full flex-col items-center justify-center text-center">
+      <div className="shadow-sm font-semibold mb-6 first-letter:shadow inline-flex rounded-lg px-4 py-1 md:py-1.5 text-sm md:text-base leading-6 text-zinc-300 bg-zinc-800 ring-1 backdrop-blur ring-white/20 hover:ring-white/30">
         Email API For Developers
       </div>
       <h1 className="text-balance md:text-7xl leading-[3.4rem] md:leading-[5rem] text-5xl font-bold tracking-tighter font-gradient">
         Resend API Email Contact Form
       </h1>
-      <p className="text-balance mt-6 text-center text-zinc-300 md:text-lg">
+      <p className="text-pretty mt-6 text-center text-zinc-300 md:text-lg leading-relaxed">
         A Simple {' '}
           <Pin href="https://nextjs.org">
             <Image
@@ -45,13 +45,13 @@ export default function Home() {
             Resend API
           </Pin>
           {' '}
-        contact form template. Styled with React-Email and built with React-Hook-Forms, validated with Zod, handles submissions with server actions, and shows toast notifications using Sonner.
+        contact form template. Styled with React-Email and built with React-Hook-Forms, validated with Zod, handles submissions with server actions, and shows toast notifications using <Link href="https://sonner.emilkowal.ski/" className="underline hover:text-white transition-all">Sonner</Link>.
       </p>
-      <p className="text-balance mt-4 text-center text-zinc-300 md:text-lg">
+      <p className="text-pretty mt-4 text-center text-zinc-300 md:text-lg">
         Yout must create an API key & verify your domain. For more info, read the  <Link href="https://www.jorge-perez.dev/blog/resend-contact-form" className="underline hover:text-white transition-all">blog.</Link>
       </p>
-      <div className="my-2 text-left mx-auto w-full max-w-xl">
-        <hr className="my-6 border-zinc-800" />
+      <hr className="my-6 border-zinc-700 w-full" />
+      <div className="text-left mx-auto w-full max-w-xl">
         <ContactForm />
       </div>
     </main>
