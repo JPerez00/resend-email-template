b6429a5ebc3ad78e4fe014f834066bd60e43755e
Jorge Perez <information.jperez@gmail.com>
2024-12-15 12:35:27 +0800

    removes 'nonempty()' deprecation warning

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,19 +1,18 @@
-// lib/schema.ts
 import { z } from 'zod'
 
 export const FormDataSchema = z.object({
-    name: z.string().nonempty('Name is required.'),
-    message: z
-        .string()
-        .nonempty('Message is required.')
-        .min(6, { message: 'Message must be at least 6 characters.' })
+  name: z.string().min(1, 'Name is required.'),
+  message: z
+    .string()
+    .min(1, 'Message is required.')
+    .min(6, { message: 'Message must be at least 6 characters.' })
 })
 
 export const ContactFormSchema = z.object({
-    name: z.string().nonempty('Name is required.'),
-    email: z.string().nonempty('Email is required.').email('Invalid email.'),
-    message: z
-        .string()
-        .nonempty('Message is required.')
-        .min(6, { message: 'Message must be at least 6 characters.' })
+  name: z.string().min(1, 'Name is required.'),
+  email: z.string().min(1, 'Email is required.').email('Invalid email.'),
+  message: z
+    .string()
+    .min(1, 'Message is required.')
+    .min(6, { message: 'Message must be at least 6 characters.' })
 })
